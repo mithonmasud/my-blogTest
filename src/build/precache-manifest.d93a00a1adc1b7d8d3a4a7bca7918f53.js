329fa36b19412a5dcd2d4795dd884e717d063707
MD Masudur Rahman <u783206@footlocker.com>
2020-07-22 08:56:41 -0400

    First commit

---
+++ src/build/precache-manifest.d93a00a1adc1b7d8d3a4a7bca7918f53.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ad53706d4102b1132675b049ae68da8b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "15f04c10ffc4e01d7b44",
+    "url": "/static/css/main.4a9d094a.chunk.css"
+  },
+  {
+    "revision": "4d40f3ad0bc1200edf09",
+    "url": "/static/js/2.aa2b1e33.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.aa2b1e33.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "15f04c10ffc4e01d7b44",
+    "url": "/static/js/main.1f3bb50f.chunk.js"
+  },
+  {
+    "revision": "8931283fa7532b66ee7f",
+    "url": "/static/js/runtime-main.fca178c7.js"
+  }
+]);
